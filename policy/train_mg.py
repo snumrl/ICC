9c0865d032fc98bb96f0c9ac1ae9dad71f62a84d
Soohwan Park <shpark1359@gmail.com>
2019-09-25 16:42:18 +0900

    add argparser in train_mg.py

--- policy/train_mg.py
+++ policy/train_mg.py
@@ -3,5 +3,6 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 if __name__ == "__main__":
-	motion_name = "walkrunfall"
-	train(motion_name)
+	parser.add_argument('-m', '--motion', help='motion name(folder name)')
+	args = parser.parse_args()
+	train(args.motion)
